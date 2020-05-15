01f7ce73824e33ff434980b15334508601a7b2f7
Arius Bronte <ariusbronte@gmail.com>
2020-05-15 21:07:30 +0000

    Implemented SignalR in Client

--- src/SignalRChat.Client/Program.cs
+++ src/SignalRChat.Client/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SignalRChat.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var connection = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:2020/chathub")
+                    .WithAutomaticReconnect()
+                    .AddJsonProtocol()
+                    .Build();
+
+                connection.On<string>("ReceiveMessage", Console.WriteLine);
+
+                await connection.StartAsync();
+
+                Console.Write("Input group name: ");
+                var groupName = Console.ReadLine();
+                
+                await connection.InvokeAsync("AddToGroup", groupName);
+                
+                while (true)
+                {
+                    await connection.InvokeAsync("Send", groupName, Console.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
